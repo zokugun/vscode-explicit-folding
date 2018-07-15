81088e3fd2c22d63b9198fe5548d804e566fee99
Baptiste Augrain <daiyam@zokugun.org>
2018-07-16 01:04:24 +0200

    - fix error in settings - update foldings on configuration changes - fix formatting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,7 @@
+## v0.1.1
+- fix error in settings
+- update foldings on configuration changes
+- fix formatting
+
 ## v0.1.0
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,47 @@ import * as vscode from 'vscode'
 
 import FoldingProvider from './foldingProvider'
 
-export function activate(context: vscode.ExtensionContext) {
-	const config = vscode.workspace.getConfiguration('folding')
+let $disposable: vscode.Disposable | null = null;
+
+function setup(context: vscode.ExtensionContext) {
+	if ($disposable !== null) {
+		$disposable.dispose();
+	}
 	
-	let provider
+	const config = vscode.workspace.getConfiguration('folding');
+	const subscriptions: vscode.Disposable[] = [];
+	
+	let provider, disposable;
 	for(let name in config) {
 		if(name === '*') {
-			provider = new FoldingProvider(config[name])
-			context.subscriptions.push(vscode.languages.registerFoldingRangeProvider({ scheme: 'file' }, provider));
-			context.subscriptions.push(vscode.languages.registerFoldingRangeProvider({ scheme: 'untitled' }, provider));
+			provider = new FoldingProvider(config[name]);
+			
+			subscriptions.push(disposable = vscode.languages.registerFoldingRangeProvider({ scheme: 'file' }, provider));
+			context.subscriptions.push(disposable);
+			
+			subscriptions.push(disposable = vscode.languages.registerFoldingRangeProvider({ scheme: 'untitled' }, provider));
+			context.subscriptions.push(disposable);
 		}
 		else {
-			provider = new FoldingProvider(config[name])
-			context.subscriptions.push(vscode.languages.registerFoldingRangeProvider({ language: name, scheme: 'file' }, provider));
-			context.subscriptions.push(vscode.languages.registerFoldingRangeProvider({ language: name, scheme: 'untitled' }, provider));
+			provider = new FoldingProvider(config[name]);
+			
+			subscriptions.push(disposable = vscode.languages.registerFoldingRangeProvider({ language: name, scheme: 'file' }, provider));
+			context.subscriptions.push(disposable);
+			
+			subscriptions.push(disposable = vscode.languages.registerFoldingRangeProvider({ language: name, scheme: 'untitled' }, provider));
+			context.subscriptions.push(disposable);
 		}
 	}
+	
+	$disposable = vscode.Disposable.from(...subscriptions);
 }
+
+export function activate(context: vscode.ExtensionContext) {
+	setup(context);
+	
+	vscode.workspace.onDidChangeConfiguration(event => {
+        if (event.affectsConfiguration('folding')) {
+			setup(context);
+        }
+    });
+};
