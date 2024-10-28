c5f40d15b6f1d51847af422f7505b12ca7b7f3ff
Baptiste Augrain <daiyam@zokugun.org>
2024-10-28 22:49:09 +0100

    fix: nudge command

--- src/commands/nudge.ts
+++ src/commands/nudge.ts
@@ -8,10 +8,4 @@ export async function nudge(): Promise<void> {
 	});
 
 	await vscode.commands.executeCommand('editor.removeManualFoldingRanges');
-
-	// await new Promise((resolve) => {
-	// 	setTimeout(resolve, 500);
-	// });
-
-	// await vscode.commands.executeCommand('workbench.action.reloadWindow');
 }
