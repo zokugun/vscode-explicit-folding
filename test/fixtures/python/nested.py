d66f4522e9fa40c450e325d05f69df4c43b1872b
Baptiste Augrain <daiyam@zokugun.org>
2024-10-29 02:20:59 +0100

    test: test foldBeforeFirstLine and nested indentation

---
+++ test/fixtures/python/nested.py
@@ -0,0 +1,36 @@
+class Foo:
+
+    # :: REGION 01 :: #
+
+    def function_01(self):
+        pass
+
+    def function_02(self):
+        pass
+
+    # :: REGION 02 :: #
+
+    def function_03(self):
+        pass
+
+    def function_04(self):
+        pass
+
+
+class Bar:
+
+    # :: REGION 03 :: #
+
+    def function_05(self):
+        pass
+
+    def function_06(self):
+        pass
+
+    # :: REGION 04 :: #
+
+    def function_07(self):
+        pass
+
+    def function_08(self):
+        pass
