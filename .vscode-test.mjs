7412f1f958d655d362c3a46b77c943232b744dc4
Baptiste Augrain <daiyam@zokugun.org>
2026-06-24 15:58:17 +0200

    build: update artifacts

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from '@vscode/test-cli'
 export default defineConfig({
 	files: '.test/test/**/*.test.js',
 	mocha: {
-      ui: 'bdd',
-      timeout: 20000
-    },
+			ui: 'bdd',
+			timeout: 20000
+		},
 })
